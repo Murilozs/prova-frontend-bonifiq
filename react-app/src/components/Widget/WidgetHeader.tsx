95555966d4518731d3a46b3983f2a71c7cfe4c1d
Murilo Zanin <murilozanins@gmail.com>
2026-05-13 15:29:16 -0300

    icon

--- react-app/src/components/Widget/WidgetHeader.tsx
+++ react-app/src/components/Widget/WidgetHeader.tsx
@@ -1,11 +1,16 @@
-import type { WidgetHeaderProps } from "../types/WidgetHeader";
+import type { WidgetHeaderProps } from "../../types/WidgetHeader";
+import bonifiqIcon from "../../assets/faviconBonifiq.png";
 
 export function WidgetHeader({ onClose }: WidgetHeaderProps) {
   return (
     <header className="widget__header">
-      <div>
-        <strong>BonifiQ</strong>
-        <span>Posts do usuário</span>
+      <div className="widget__brand">
+        <img src={bonifiqIcon} alt="BonifiQ" />
+
+        <div>
+          <strong>BonifiQ</strong>
+          <span>Posts do usuário</span>
+        </div>
       </div>
 
       <button onClick={onClose} aria-label="Fechar widget">
